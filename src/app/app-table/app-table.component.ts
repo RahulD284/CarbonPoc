a6692e6ef0e2af4e044b5d0e7305fd1ae6a11404
Akash kumar <akash@Akashs-Air.bbrouter>
2024-06-25 20:48:01 +0530

    added pagination code

--- src/app/app-table/app-table.component.ts
+++ src/app/app-table/app-table.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { TableModel, TableItem, TableHeaderItem } from 'carbon-components-angular';
+import { TableModel, TableItem, TableHeaderItem, PaginationModel } from 'carbon-components-angular';
 
 @Component({
   selector: 'app-app-table',
@@ -12,32 +12,55 @@ export class AppTableComponent implements OnInit {
   @Input() stickyHeader = false;
   @Input() size: 'sm' | 'md' | 'lg' = 'md';
   @Input() skeleton = false;
+  @Input() showPageInput = true;
   @Input() enableSingleSelect = false;
   @Input() showSelectionColumn = false;
   @Input() striped = false;
   @Input() sortable = false;
   @Input() isDataGrid = false;
+  fullData: TableItem[][] = [];
+  pageData: TableItem[][] = [];
+  paginationModel = new PaginationModel();
+  pageSize = 10;
+  currentPage = 1;
+
 
   constructor(private http: HttpClient) {}
   
   ngOnInit() {
-    if (!this.model) {
-      this.model = new TableModel();
-    }
     this.loadTableData();
   }
   loadTableData() {
     this.http.get<any>('assets/data.json').subscribe(data => {
-      // Load header data
+      
       this.model.header = data.header.map((header: string) => new TableHeaderItem({ data: header }));
 
-      // Load row data
-      this.model.data = data.data.map((row: any[]) => row.map(item => new TableItem({ data: item })));
+      this.fullData = data.data.map((row: any[]) => row.map(item => new TableItem({ data: item })));
+      this.paginationModel.totalDataLength = this.fullData.length;
+      this.updatePage();
     });
   }
-  onRowClick(row: any) {
-    // Handle row click event here
-    console.log('Row clicked:', row);
-    // You can navigate to another page or perform any action based on the clicked row
+  updatePage() {
+    this.pageData = this.fullData.slice(this.currentPage * 10 - 10, this.currentPage * 10);
+    this.model.data = this.pageData;
+    console.log('Page data:', this.pageData);
   }
+
+  onPageChange(event: any) {
+    this.currentPage = event.page;
+    this.updatePage();
+    console.log("event from pageChange",event)
+  }
+
+  onPageSizeChange(event: any) {
+    this.pageSize = event.pageSize;
+    this.currentPage = 1;
+    this.updatePage();
+    console.log("event from onPageSizeChange",event)
+  }
+  selectPage(page: any) {
+		this.currentPage = page;
+    this.updatePage();
+    console.log("page from selectPage",page)
+	}
 }
